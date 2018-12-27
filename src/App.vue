90e7242508d4d561c704357a2dbe4544546ce5a2
Slapec <slapec@gmail.com>
2018-12-27 13:38:57 +0100

    Playback works

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,65 @@
 <template>
-  <div id="app">
-    <!--<div id="nav">-->
-      <!--<router-link to="/">Home</router-link> |-->
-      <!--<router-link to="/about">About</router-link>-->
-    <!--</div>-->
-    <!--<router-view/>-->
-    <h1>{{ title }}</h1>
-    <audio v-if="file" :src="file" autoplay controls style="width: 100%"></audio>
-  </div>
+    <div id="app">
+        <playlist
+                id="playlist-main"
+                :api="'/api/v1/media/audio/'"
+                @select="play"
+        />
+        <player
+                id="player-main"
+                :media="currentMedia"
+        />
+    </div>
 </template>
 
 <script>
+  import Playlist from '@/components/Playlist.vue';
+  import Player from '@/components/Player.vue';
+
   export default {
+    components: {
+      Playlist,
+      Player
+    },
     data(){
       return {
-        title: '',
-        file: null
+        currentMedia: null
       }
     },
-    async mounted(){
-      const tracks = await fetch('/api/v1/media/audio/').then(r => r.json());
-      this.title = tracks[0].title;
-      this.file = tracks[0].url;
+    methods: {
+      play(media){
+        this.currentMedia = media;
+      }
     }
-  }
+  };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+    html, body {
+        padding: 0;
+        margin: 0;
     }
-  }
-}
+
+    #app {
+        position: fixed;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        overflow: hidden;
+        box-sizing: border-box;
+        display: flex;
+        background: #e3e3e3;
+        flex-direction: column;
+    }
+
+    #playlist-main {
+        flex-grow: 1;
+        overflow: auto;
+    }
+
+    #player-main {
+        border-top: 2px solid black;
+        flex-shrink: 0;
+    }
 </style>
